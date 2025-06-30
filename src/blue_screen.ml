bbf5e1dc54f3fa00e13d86a63580a13c8294763d
Joseph <josephor@mit.edu>
2025-06-30 20:45:49 +0000

    complete blue_screen and dither

--- src/blue_screen.ml
+++ src/blue_screen.ml
@@ -5,11 +5,50 @@ open Core
    the corresponding position in the background image instead of
    just ignoring the background image and returning the foreground image.
 *)
-let transform ~foreground ~background:_ = foreground
+let transform ~foreground ~background =
+  Image.mapi foreground ~f:(fun ~x ~y (r, g, b) ->
+    if b > r + g then Image.get background ~x ~y else r, g, b)
+;;
+
+let%expect_test "blue_screen" =
+  let expected_image =
+    Image.load_ppm
+      ~filename:"/home/ubuntu/raster/images/reference-oz_bluescreen_vfx.ppm"
+  in
+  let original_image =
+    Image.load_ppm ~filename:"/home/ubuntu/raster/images/oz_bluescreen.ppm"
+  in
+  let new_background =
+    Image.load_ppm ~filename:"/home/ubuntu/raster/images/meadow.ppm"
+  in
+  let our_output_image =
+    transform ~foreground:original_image ~background:new_background
+  in
+  let x_coords = Image.width our_output_image |> List.init ~f:(fun x -> x) in
+  let y_coords =
+    Image.height our_output_image |> List.init ~f:(fun y -> y)
+  in
+  List.iter x_coords ~f:(fun x_coord ->
+    List.iter y_coords ~f:(fun y_coord ->
+      if
+        Pixel.equal
+          (Image.get our_output_image ~x:x_coord ~y:y_coord)
+          (Image.get expected_image ~x:x_coord ~y:y_coord)
+      then ()
+      else
+        print_string
+          ("incorrect at pixel at "
+           ^ string_of_int x_coord
+           ^ ", "
+           ^ string_of_int y_coord
+           ^ "\n")));
+  [%expect]
+;;
 
 let command =
   Command.basic
-    ~summary:"Replace the 'blue' pixels of an image with those from another image"
+    ~summary:
+      "Replace the 'blue' pixels of an image with those from another image"
     [%map_open.Command
       let foreground_file =
         flag
@@ -28,5 +67,7 @@ let command =
         let image' = transform ~foreground ~background in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn foreground_file ~suffix:".ppm" ^ "_vfx.ppm")]
+          ~filename:
+            (String.chop_suffix_exn foreground_file ~suffix:".ppm"
+             ^ "_vfx.ppm")]
 ;;
--- src/blur.ml
+++ src/blur.ml
@@ -2,7 +2,48 @@ open Core
 
 (* You need to modify this function to blur the input image
    based on the provided radius instead of ignoring it. *)
-let transform image ~radius:_ = image
+let transform image ~radius =
+  let height = Image.height image in
+  let width = Image.width image in
+  let blank_image = Image.make ~max_val:65535 ~height ~width (0, 0, 0) in
+  Image.mapi blank_image ~f:(fun ~x ~y _ ->
+    let x_start = max 0 (x - radius) in
+    let x_end = min (width - 1) (x + radius) in
+    let y_start = max 0 (y - radius) in
+    let y_end = min (height - 1) (y + radius) in
+    Image.slice image ~x_start ~x_end ~y_start ~y_end |> Image.mean_pixel)
+;;
+
+let%expect_test "blur" =
+  let expected_image =
+    Image.load_ppm
+      ~filename:
+        "/home/ubuntu/raster/images/reference-beach_portrait_blur.ppm"
+  in
+  let original_image =
+    Image.load_ppm ~filename:"/home/ubuntu/raster/images/beach_portrait.ppm"
+  in
+  let our_output_image = transform original_image ~radius:3 in
+  let x_coords = Image.width our_output_image |> List.init ~f:(fun x -> x) in
+  let y_coords =
+    Image.height our_output_image |> List.init ~f:(fun y -> y)
+  in
+  List.iter x_coords ~f:(fun x_coord ->
+    List.iter y_coords ~f:(fun y_coord ->
+      if
+        Pixel.equal
+          (Image.get our_output_image ~x:x_coord ~y:y_coord)
+          (Image.get expected_image ~x:x_coord ~y:y_coord)
+      then ()
+      else
+        print_string
+          ("incorrect at pixel at "
+           ^ string_of_int x_coord
+           ^ ", "
+           ^ string_of_int y_coord
+           ^ "\n")));
+  [%expect]
+;;
 
 let command =
   Command.basic
@@ -24,5 +65,6 @@ let command =
         let image' = transform image ~radius in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
 ;;
